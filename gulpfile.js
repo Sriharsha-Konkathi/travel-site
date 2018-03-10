cba7f809bdb09b3da52daa229c2c2c4ccb73d917
Sriharsha <k.sriharsha0001@gmail.com>
2018-03-10 12:18:40 +0530

    Commited with gulp changes and BEM

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,12 @@
-var gulp = require('gulp'),
-watch = require('gulp-watch'),
-postcss = require('gulp-postcss'),
-autoprefixer = require('autoprefixer'),
-cssvars = require('postcss-simple-vars'),
-nested = require('postcss-nested');
+require('./gulp/tasks/styles');
+require('./gulp/tasks/watch');
 
 
-gulp.task('default',function(){
-    console.log("hooray u created a gulp task");
-});
 
-gulp.task('html',function(){
-    console.log("lets do something useful to your html");
-});
 
-gulp.task('styles',function(){
-    return gulp.src('./app/assets/styles/styles.css')
-    .pipe(postcss([cssvars,nested,autoprefixer]))
-    .pipe(gulp.dest('./app/temp/styles'));
-});
 
-gulp.task('watch',function(){
 
-    watch('./app/index.html',function(){
-        gulp.start('html');
-    });
 
-    watch('./app/assets/styles/**/*.css',function(){
-        gulp.start('styles');
-    });
-});
+
+
+
