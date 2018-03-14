af242d5efe91dfa41e1453c6e26f7013d3af0a40
Sriharsha <k.sriharsha0001@gmail.com>
2018-03-14 23:08:00 +0530

    Worked with the toggle menu and is now able to click it

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,19 +1,3 @@
-var $ = require('jquery');
-//var Person = require('./modules/Person');
-import Person from './modules/Person'; //ES6 way of importing the module
+import MobileMenu from './modules/MobileMenu';
 
-class Adult extends Person {
-    payTax() {
-        console.log(this.name + " now owes $0 in taxes. ");
-    }
-}
-
-alert("this is testing 123 and again");
-
-var john = new Person("John Doe","blue");
-john.greet();
-var jane = new Adult("Jane Smith","orange");
-jane.greet();
-jane.payTax();
-
-$("h1").remove();
+var mobileMenu = new MobileMenu();
